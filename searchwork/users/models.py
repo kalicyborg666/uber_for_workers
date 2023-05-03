5ef5e6e4330a70da32bbd760770031cb00d11bf3
joi <cybervabe666@gmail.com>
2023-05-03 22:30:10 +0500

    add REQUIRED_FIELDS, del fields photo, city del model City

--- searchwork/users/models.py
+++ searchwork/users/models.py
@@ -2,18 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class City(models.Model):
-    name = models.CharField(max_length=300)
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class CustomUser(AbstractUser):
-    city = models.ForeignKey('City', on_delete=models.PROTECT,
-                             related_name='user_city')
     age = models.PositiveSmallIntegerField()
     date_born = models.DateField()
+
+    REQUIRED_FIELDS = ['email', 'age', 'date_born']
 
     def __str__(self) -> str:
         return self.username
